721ac4d469cca2b82ea7512305a99a0d9cb727a5
hazim1093 <hazim_malik@hotmail.com>
2017-01-31 10:57:49 +0500

    Add License header

--- scripts/merge-extra-cloud-config-units.py
+++ scripts/merge-extra-cloud-config-units.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
+###############################################################################
+# Copyright 2016 Aurora Solutions
+#
+#    http://www.aurorasolutions.io
+#
+# Aurora Solutions is an innovative services and product company at
+# the forefront of the software industry, with processes and practices
+# involving Domain Driven Design(DDD), Agile methodologies to build
+# scalable, secure, reliable and high performance products.
+#
+# Stakater is an Infrastructure-as-a-Code DevOps solution to automate the
+# creation of web infrastructure stack on Amazon. Stakater is a collection
+# of Blueprints; where each blueprint is an opinionated, reusable, tested,
+# supported, documented, configurable, best-practices definition of a piece
+# of infrastructure. Stakater is based on Docker, CoreOS, Terraform, Packer,
+# Docker Compose, GoCD, Fleet, ETCD, and much more.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+###############################################################################
+# Merge extra cloud-config units
+# This script merges extra cloud config units into the cloud-config template
+# and creates a cloud-config.yaml file
+# Authors: Hazim
+###############################################################################
 import argparse
 
 argParse = argparse.ArgumentParser()
